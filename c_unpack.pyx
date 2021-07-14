d81ee8459edc7fe0c1a41ff5ce989a2418275585
Nikhil Marathe <nsm.nikhil@gmail.com>
2021-07-13 20:33:50 -0700

    The compiled one gives even more improvement

--- c_unpack.pyx
+++ c_unpack.pyx
@@ -3,5 +3,14 @@ import struct
 def cython_unpack(format, buf):
     struct.unpack(format, buf)
 
+def cython_unpack_from(format, buf):
+    struct.unpack_from(format, buf, 0)
+
 def cython_unpack_typed(str format, const unsigned char[:] buf):
     struct.unpack(format, buf)
+
+def get_compiled_struct_obj(format):
+    return struct.Struct(format)
+
+def cython_compiled_unpack(struct_obj, buf):
+    struct_obj.unpack(buf)
--- main.py
+++ main.py
@@ -11,29 +11,58 @@ def create_buffer():
 def plain_unpack(buf):
     struct.unpack(FORMAT, buf)
 
+def compiled_unpack(struct_obj, buf):
+    struct_obj.unpack(buf)
+
+def plain_unpack_from(buf):
+    struct.unpack_from(FORMAT, buf, 0)
+
 def main():
     setup = """
+import struct
+
 from __main__ import (
     create_buffer,
     plain_unpack,
+    plain_unpack_from,
+    compiled_unpack,
 )
 from c_unpack import (
     cython_unpack,
+    cython_unpack_from,
     cython_unpack_typed,
+    cython_compiled_unpack,
+
+    get_compiled_struct_obj,
 )
-buf = create_buffer()"""
+
+buf = create_buffer()
+struct_obj = struct.Struct('{format}')
+cython_struct_obj = get_compiled_struct_obj('{format}')""".format(format=FORMAT)
 
     tests = [
         ("plain unpack()", "plain_unpack({})"),
+
+        ("plain unpack_from()", "plain_unpack_from({})"),
         ("cython unpack()", "cython_unpack('{}', {{}})".format(FORMAT)),
+        ("cython unpack_from()", "cython_unpack_from('{}', {{}})".format(FORMAT)),
         ("cython typed unpack()", "cython_unpack_typed('{}', {{}})".format(FORMAT)),
+
+        ("plain compiled unpack()", "compiled_unpack(struct_obj, {})"),
+        ("cython compiled unpack()", "cython_compiled_unpack(cython_struct_obj, {})"),
     ]
 
     print("---------------------------------------------")
     print("Test Name                      Time (s)")
     print("---------------------------------------------")
-    for test_name, test_snippet in tests:
-        print("{:30} {:>4.3f}".format(test_name, timeit.timeit(test_snippet.format("buf"), setup=setup, number=N)))
+    baseline = 0
+    for i, (test_name, test_snippet) in enumerate(tests):
+        took = timeit.timeit(test_snippet.format("buf"), setup=setup, number=N)
+        if i == 0:
+            baseline = took
+            print("{:30} {:>4.3f}".format(test_name, took))
+        else:
+            print("{:30} {:>4.3f} ({:>+.2%})".format(test_name, took, took / baseline))
 
 if __name__ == '__main__':
     main()
